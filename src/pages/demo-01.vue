e8d4efee060387ab139a16b1a1de2d6686ce82ba
xiaoqiang-zhao <longze_xq@163.com>
2020-03-06 19:58:30 +0800

    添加航拍模型

--- src/pages/demo-01.vue
+++ src/pages/demo-01.vue
@@ -1,17 +1,81 @@
 <template>
     <div class="standard-base markdown">
         <markdown-doc-content :mdContent="mdContent"/>
-        
+        <div id="canvas-container"></div>
     </div>
 </template>
 
 <script>
+import * as THREE from 'three/build/three.module.js';
 import markdownDocContent from '@/components/markdown-doc-content.vue';
 import baseMdContent from './demo-01.md';
 
 export default {
     components: {
         markdownDocContent
+    },
+    mounted() {
+        // 场景
+        var scene = new THREE.Scene();
+        // 摄像机
+        const space = 208;
+        const width = window.innerWidth - space;
+        const height = window.innerHeight - space;
+        var camera = new THREE.PerspectiveCamera(75, width/height, 0.1, 1000);
+        camera.position.set(0, 0, 5);
+        camera.lookAt(0, 0, 0);
+
+        // 渲染器
+        var renderer = new THREE.WebGLRenderer({
+            // 抗锯齿
+            antialias: true
+        });
+        // 设置大小
+        renderer.setSize(width, height);
+        // 添加渲染器生成的 canvas 到页面的 body 中
+        const canvasContainer = document.getElementById('canvas-container');
+        canvasContainer.appendChild(renderer.domElement);
+
+        // 材质
+        var material = new THREE.MeshStandardMaterial({
+            color: 0x4169E1
+        });
+
+        // 几何图形 - 立方体
+        var box = new THREE.BoxGeometry(1, 1, 1, 10);
+        var cube = new THREE.Mesh(box, material);
+        cube.position.set(-2, 0, 0);
+        scene.add(cube);
+
+        // 几何图形 - 圆柱
+        var cylinderGeometry = new THREE.CylinderGeometry(0.3, 0.3, 3, 100);
+        var cylinder = new THREE.Mesh(cylinderGeometry, material);
+        cylinder.position.set(2, 0, 0);
+        scene.add(cylinder);
+
+        // 光 - 环境光使物体整体可见
+        var light = new THREE.AmbientLight(0x404040); // soft white light
+        scene.add(light);
+        // 光 - 平行光展示阴影
+        var directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+        directionalLight.position.set(0, 0, 5)
+        scene.add(directionalLight);
+
+        // var render = function () {
+        //     requestAnimationFrame(render);
+
+        //     cube.rotation.x += 0.01;
+        //     cube.rotation.y += 0.01;
+
+        //     cylinder.rotation.x += 0.01;
+        //     cylinder.rotation.y += 0.01;
+
+        //     renderer.render(scene, camera);
+        // };
+
+        // render();
+
+        renderer.render(scene, camera);
     },
     data() {
         return {
